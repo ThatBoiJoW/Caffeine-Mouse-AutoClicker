86b67699cd333d2bb14ae6f99ffada6f58f16cad
Jowinley Cimeus <jowinley.thecimeus@gmail.com>
2025-10-10 23:07:48 -0400

    Initial commit with proper .gitignore

---
+++ Caffine Mouse Autoclicker/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace Caffine_Mouse_Autoclicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CafMousePic = new System.Windows.Forms.PictureBox();
+            this.lblInterval = new System.Windows.Forms.GroupBox();
+            this.numMilSec = new System.Windows.Forms.NumericUpDown();
+            this.numSec = new System.Windows.Forms.NumericUpDown();
+            this.numMin = new System.Windows.Forms.NumericUpDown();
+            this.numHours = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblHotKey = new System.Windows.Forms.GroupBox();
+            this.comboHotKeyStop = new System.Windows.Forms.ComboBox();
+            this.comboHotKeyStart = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboAction = new System.Windows.Forms.ComboBox();
+            this.comboMouse = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnRegisterHotkeys = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CafMousePic)).BeginInit();
+            this.lblInterval.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numMilSec)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSec)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHours)).BeginInit();
+            this.lblHotKey.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CafMousePic
+            // 
+            this.CafMousePic.Image = ((System.Drawing.Image)(resources.GetObject("CafMousePic.Image")));
+            this.CafMousePic.InitialImage = null;
+            this.CafMousePic.Location = new System.Drawing.Point(22, 12);
+            this.CafMousePic.Name = "CafMousePic";
+            this.CafMousePic.Size = new System.Drawing.Size(287, 237);
+            this.CafMousePic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.CafMousePic.TabIndex = 0;
+            this.CafMousePic.TabStop = false;
+            // 
+            // lblInterval
+            // 
+            this.lblInterval.Controls.Add(this.numMilSec);
+            this.lblInterval.Controls.Add(this.numSec);
+            this.lblInterval.Controls.Add(this.numMin);
+            this.lblInterval.Controls.Add(this.numHours);
+            this.lblInterval.Controls.Add(this.label8);
+            this.lblInterval.Controls.Add(this.label7);
+            this.lblInterval.Controls.Add(this.label6);
+            this.lblInterval.Controls.Add(this.label5);
+            this.lblInterval.Location = new System.Drawing.Point(315, 61);
+            this.lblInterval.Name = "lblInterval";
+            this.lblInterval.Size = new System.Drawing.Size(411, 171);
+            this.lblInterval.TabIndex = 1;
+            this.lblInterval.TabStop = false;
+            this.lblInterval.Text = "Click Interval";
+            // 
+            // numMilSec
+            // 
+            this.numMilSec.Location = new System.Drawing.Point(305, 59);
+            this.numMilSec.Name = "numMilSec";
+            this.numMilSec.Size = new System.Drawing.Size(69, 26);
+            this.numMilSec.TabIndex = 7;
+            // 
+            // numSec
+            // 
+            this.numSec.Location = new System.Drawing.Point(218, 58);
+            this.numSec.Name = "numSec";
+            this.numSec.Size = new System.Drawing.Size(68, 26);
+            this.numSec.TabIndex = 6;
+            // 
+            // numMin
+            // 
+            this.numMin.Location = new System.Drawing.Point(119, 59);
+            this.numMin.Name = "numMin";
+            this.numMin.Size = new System.Drawing.Size(65, 26);
+            this.numMin.TabIndex = 5;
+            // 
+            // numHours
+            // 
+            this.numHours.Location = new System.Drawing.Point(27, 59);
+            this.numHours.Name = "numHours";
+            this.numHours.Size = new System.Drawing.Size(57, 26);
+            this.numHours.TabIndex = 4;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(301, 35);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 20);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "MiliSeconds";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(214, 35);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 20);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Seconds";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(115, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 20);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Minutes";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Hours";
+            // 
+            // lblHotKey
+            // 
+            this.lblHotKey.Controls.Add(this.comboHotKeyStop);
+            this.lblHotKey.Controls.Add(this.comboHotKeyStart);
+            this.lblHotKey.Controls.Add(this.label2);
+            this.lblHotKey.Controls.Add(this.label1);
+            this.lblHotKey.Location = new System.Drawing.Point(12, 255);
+            this.lblHotKey.Name = "lblHotKey";
+            this.lblHotKey.Size = new System.Drawing.Size(336, 127);
+            this.lblHotKey.TabIndex = 2;
+            this.lblHotKey.TabStop = false;
+            this.lblHotKey.Text = "Hot key";
+            this.lblHotKey.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // comboHotKeyStop
+            // 
+            this.comboHotKeyStop.FormattingEnabled = true;
+            this.comboHotKeyStop.Location = new System.Drawing.Point(176, 75);
+            this.comboHotKeyStop.Name = "comboHotKeyStop";
+            this.comboHotKeyStop.Size = new System.Drawing.Size(121, 28);
+            this.comboHotKeyStop.TabIndex = 5;
+            // 
+            // comboHotKeyStart
+            // 
+            this.comboHotKeyStart.FormattingEnabled = true;
+            this.comboHotKeyStart.Location = new System.Drawing.Point(176, 35);
+            this.comboHotKeyStart.Name = "comboHotKeyStart";
+            this.comboHotKeyStart.Size = new System.Drawing.Size(121, 28);
+            this.comboHotKeyStart.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Stop: CTRL +";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Start: CTRL +";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.comboAction);
+            this.groupBox3.Controls.Add(this.comboMouse);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(363, 255);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(363, 127);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Mouse Action";
+            // 
+            // comboAction
+            // 
+            this.comboAction.FormattingEnabled = true;
+            this.comboAction.Location = new System.Drawing.Point(147, 74);
+            this.comboAction.Name = "comboAction";
+            this.comboAction.Size = new System.Drawing.Size(121, 28);
+            this.comboAction.TabIndex = 5;
+            // 
+            // comboMouse
+            // 
+            this.comboMouse.FormattingEnabled = true;
+            this.comboMouse.Location = new System.Drawing.Point(147, 35);
+            this.comboMouse.Name = "comboMouse";
+            this.comboMouse.Size = new System.Drawing.Size(121, 28);
+            this.comboMouse.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Action";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mouse";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(22, 412);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(107, 35);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(151, 412);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(121, 35);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(604, 412);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(106, 36);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit&";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnRegisterHotkeys
+            // 
+            this.btnRegisterHotkeys.Location = new System.Drawing.Point(315, 413);
+            this.btnRegisterHotkeys.Name = "btnRegisterHotkeys";
+            this.btnRegisterHotkeys.Size = new System.Drawing.Size(204, 35);
+            this.btnRegisterHotkeys.TabIndex = 7;
+            this.btnRegisterHotkeys.Text = "Register Hotkey";
+            this.btnRegisterHotkeys.UseVisualStyleBackColor = true;
+            this.btnRegisterHotkeys.Click += new System.EventHandler(this.btnRegisterHotkeys_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 471);
+            this.Controls.Add(this.btnRegisterHotkeys);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.lblHotKey);
+            this.Controls.Add(this.lblInterval);
+            this.Controls.Add(this.CafMousePic);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Caffeine Mouse Autoclicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CafMousePic)).EndInit();
+            this.lblInterval.ResumeLayout(false);
+            this.lblInterval.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numMilSec)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSec)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHours)).EndInit();
+            this.lblHotKey.ResumeLayout(false);
+            this.lblHotKey.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox CafMousePic;
+        private System.Windows.Forms.GroupBox lblInterval;
+        private System.Windows.Forms.GroupBox lblHotKey;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown numMin;
+        private System.Windows.Forms.NumericUpDown numHours;
+        private System.Windows.Forms.NumericUpDown numMilSec;
+        private System.Windows.Forms.NumericUpDown numSec;
+        private System.Windows.Forms.ComboBox comboHotKeyStop;
+        private System.Windows.Forms.ComboBox comboHotKeyStart;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ComboBox comboAction;
+        private System.Windows.Forms.ComboBox comboMouse;
+        private System.Windows.Forms.Button btnRegisterHotkeys;
+    }
+}
+
---
+++ Caffine Mouse Autoclicker/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;   // Needed for API function calls
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caffine_Mouse_Autoclicker
+{
+    public partial class Form1 : Form
+    {
+        // Import Windows API funtion for simulating mouse events 
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        // Import Windows API functions for registering and unregistering hotkeys
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, Keys vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        const int MODE_NONE = 0x0000;
+        const int WM_HOTKEY = 0x0312;
+
+        // Mouse event flags for mouse_event API
+        private const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const uint MOUSEEVENTF_LEFTUP = 0x0004;
+        private const uint MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        private const uint MOUSEEVENTF_RIGHTUP = 0x0010;
+
+        // Hotkeys IDs for indetification when handling key presses
+        private const int HOTKEY_ID_START = 1;
+        private const int HOTKEY_ID_STOP = 2;
+
+        // Timer that triggers mouse clicks
+        Timer clickerTimer = new Timer();
+
+        // Variables to store chosen start and stop hotkeys
+        Keys startHotkey;
+        Keys stopHotkey;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Populate hotkey combo boxes with all availale keys 
+            foreach (Keys key in Enum.GetValues(typeof(Keys)))
+            {
+                comboHotKeyStart.Items.Add(key);
+                comboHotKeyStop.Items.Add(key);
+            }
+
+            // Set default hotkeys
+            comboHotKeyStart.SelectedItem = Keys.F6;
+            comboHotKeyStop.SelectedItem = Keys.F7;
+
+            // Populate mouse button selection
+            comboMouse.Items.Add("Left");
+            comboMouse.Items.Add("Right");
+            comboMouse.SelectedIndex = 0;
+
+            // Populate click type selection
+            comboAction.Items.Add("Click");
+            comboAction.Items.Add("Double Click");
+            comboAction.SelectedIndex = 0;
+
+            // Assign click action to timer tick event
+            clickerTimer.Tick += (s, e) => PerformClick();
+        }
+
+
+        private void ClickTimer_Tick(object sender, EventArgs e)
+        {
+            PerformClick();
+        }
+
+        // Executes a mouse click (single or double, left or right) based on user selection.
+        private void PerformClick()
+        {
+            bool isLeft = comboMouse.SelectedItem.ToString() == "Left";
+            bool isDouble = comboAction.SelectedItem.ToString() == "Double Click";
+
+            uint down = isLeft ? MOUSEEVENTF_LEFTDOWN : MOUSEEVENTF_RIGHTDOWN;
+            uint up = isLeft ? MOUSEEVENTF_LEFTUP : MOUSEEVENTF_RIGHTUP;
+
+            mouse_event(down, 0, 0, 0, 0);
+            mouse_event(up, 0, 0, 0, 0);
+
+            if (isDouble)
+            {
+                mouse_event(down, 0, 0, 0, 0);
+                mouse_event(up, 0, 0, 0, 0);
+            }
+        }
+
+        // Starts the autoclicker with the specified interval
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int interval = (int)(numHours.Value * 3600000 +
+                                 numMin.Value * 60000 +
+                                 numSec.Value * 1000 +
+                                 numMilSec.Value);
+
+            if (interval <= 0)
+            {
+                MessageBox.Show("Please set a valid interval PLEEEEAAASSE.");
+                return;
+            }
+
+            clickerTimer.Interval = interval;
+            clickerTimer.Start();
+        }
+
+        // Stops the autoclicker
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            clickerTimer.Stop();
+        }
+
+
+        // Empty placeholder event handlers
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == startHotkey)
+            {
+                btnStart.PerformClick();
+            }
+
+            else if (e.KeyCode == stopHotkey)
+            {
+                btnStop.PerformClick();
+            }
+        }
+
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        // Alternative method for clicking mouse (similar to PerformClick).
+        private void ClickMouse(object sender, EventArgs e)
+        {
+            bool isLeft = comboMouse.SelectedItem.ToString() == "Left";
+            bool isDouble = comboAction.SelectedItem.ToString() == "Double Click";
+
+            if (isLeft)
+            {
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                if (isDouble)
+                    mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            }
+            else
+            {
+                mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+                if (isDouble)
+                    mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+            }
+        }
+
+
+        // Register hotkeys fo starting and stopping the autoclicker.
+        private void btnRegisterHotkeys_Click(object sender, EventArgs e)
+        {
+            UnregisterHotKey(this.Handle, HOTKEY_ID_START);
+            UnregisterHotKey(this.Handle, HOTKEY_ID_STOP);
+
+            Keys startHotKey = (Keys)comboHotKeyStart.SelectedItem;
+            Keys stopHotKey = (Keys)comboHotKeyStop.SelectedItem;
+
+            RegisterHotKey(this.Handle, HOTKEY_ID_START, 0, startHotKey);
+            RegisterHotKey(this.Handle, HOTKEY_ID_STOP, 0, stopHotKey);
+
+            MessageBox.Show($"Hotkeys Registered:\nStart = {startHotKey}\nStop = {stopHotKey}");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            clickerTimer.Stop();        // Stop timer before exiting
+            this.Close();
+        }
+
+        // Override WndProc to handle global hotkey presses
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_HOTKEY)
+            {
+                int id = m.WParam.ToInt32();
+                if (id == HOTKEY_ID_START)
+                {
+                    btnStart.PerformClick();
+                }
+                else if (id == HOTKEY_ID_STOP)
+                {
+                    btnStop.PerformClick();
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        // Unregister hotkeys when the form closes
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, HOTKEY_ID_START);
+            UnregisterHotKey(this.Handle, HOTKEY_ID_STOP);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
